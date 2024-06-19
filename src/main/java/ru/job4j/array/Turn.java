0d3f9d1af3622badea6bcefd5cc038b420717140
magahon-f <ma.trufakina@gmail.com>
2024-06-19 21:38:59 +0300

    6.2.3. Перевернуть массив. [#226790 #500730] цикл до середины массива

--- src/main/java/ru/job4j/array/Turn.java
+++ src/main/java/ru/job4j/array/Turn.java
@@ -2,7 +2,7 @@ package ru.job4j.array;
 
 public class Turn {
     public static int[] back(int[] array) {
-        for (int source = 0; source < array.length - source; source++) {
+        for (int source = 0; source < array.length / 2; source++) {
             int destination = array.length - source - 1;
             int temp = array[source];
             array[source] = array[destination];
