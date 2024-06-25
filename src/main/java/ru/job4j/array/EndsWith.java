b4b8b8a5def3273291968084a0bf57f546164687
magahon-f <ma.trufakina@gmail.com>
2024-06-25 14:07:50 +0300

    6.4.3. Слово заканчивается на ... [#156317 #502259] убраны лишние переменные, исправлен цикл на прямой проход

--- src/main/java/ru/job4j/array/EndsWith.java
+++ src/main/java/ru/job4j/array/EndsWith.java
@@ -3,14 +3,10 @@ package ru.job4j.array;
 public class EndsWith {
     public static boolean endsWith(char[] word, char[] postfix) {
         boolean result = true;
-        int sizeWord = word.length - 1;
-        int sizePostfix = postfix.length - 1;
-        for (int i = sizePostfix; i >= 0; i--) {
-            if (word[sizeWord] != postfix[i]) {
+        for (int i = 0; i < postfix.length; i++) {
+            if (word[word.length - postfix.length + i] != postfix[i]) {
                 result = false;
                 break;
-            } else {
-                sizeWord--;
             }
         }
         return result;
